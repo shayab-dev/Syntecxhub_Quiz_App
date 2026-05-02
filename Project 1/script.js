26e24127ebddd86bfcfeb1794bc9052120facfa4
Shayab Rahman <shayabrahman354@gmail.com>
2026-05-02 13:16:29 +0530

    commit

---
+++ Project 1/script.js
@@ -0,0 +1,42 @@
+const questions = [
+  {
+    question: 'Which is the largest animal in the world?',
+    answers: [
+      { text: 'Shark', correct: false },
+      { text: 'Blue Whale', correct: true },
+      { text: 'Elephant', correct: false },
+      { text: 'Girraffe', correct: false },
+    ]
+  },
+
+  {
+     question: 'Which is the smallest continent in the world?',
+    answers: [
+      { text: 'Asia', correct: false },
+      { text: 'Australia', correct: true },
+      { text: 'Arctic', correct: false },
+      { text: 'Africa', correct: false },
+    ]
+  },
+
+  {
+     question: 'Which is the largest desert in the world?',
+    answers: [
+      { text: 'Kalahari', correct: false },
+      { text: 'Gobi', correct: false },
+      { text: 'Sahara', correct: false },
+      { text: 'Antartica', correct: faltruese },
+    ]
+  },
+
+  {
+    question: 'How many districts are there in West Bengal ?',
+    answers: [
+      { text: '21', correct: false },
+      { text: '22', correct: false },
+      { text: '23', correct: true },
+      { text: '24', correct: false },
+    ]
+  }
+  
+];
